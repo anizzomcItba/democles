ba6e9b0df1d3373ee947f46688f5c4889c9a6d27
anizzomc <anizzomc@83e78514-4733-11de-969e-ebe3073e72ec>
2009-09-25 12:04:17 +0000

    Borrado

--- trunk/Democles/include/kernel.h
+++
@@ -1,3 +0,0 @@
-#include"defs.h"
-
-void setup_IDT_entry (DESCR_INT *item, byte selector, dword offset, byte access, byte cero);
--- trunk/Democles/include/string.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef _STRING_H_
-#define _STRING_H_
-
-void *memcpy(void *dst, const void *src, size_t qty);
-
-#endif
