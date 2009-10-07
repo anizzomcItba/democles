81b003ed9e32f16a749c360ff7c51bc317ef1b13
anizzomc <anizzomc@83e78514-4733-11de-969e-ebe3073e72ec>
2009-10-07 12:30:58 +0000

    Modularizado un poco más el tp....
    sched.c debería tener el scheduler.
    IO.c todas las funciones de io...
    las llamadas a "moveCursor" o esas cosas, deberían pasar por otra interrupción... tal vez la 79. Es para que quede
    manejo interno a que página de video corresponde cada acción.

--- trunk/Damocles/include/sys.h
+++ trunk/Damocles/include/sys.h
@@ -3,14 +3,7 @@
 #ifndef _DEFS_H_
 #define _DEFS_H_
 
-
-void syswrite(int fd, char * buffer, size_t qty);
-void sysread(int fd, char * buffer, size_t qty);
-int sysflush(int fd);
 void timer_tick_handler();
 
-void fdTableInit();
-
-void bufferAdd(int fd, char input);
 
 #endif
---
+++ trunk/Damocles/sched.c
@@ -0,0 +1,18 @@
+
+#include "include/sched.h"
+#include "include/defs.h"
+
+#define KEYBOARD 8
+
+
+int schedGetGlobalFd(int fd){
+	switch(fd){
+	case STDOUT: return 0;
+	//8 Terminales
+	case STDIN: return KEYBOARD;
+	case CURSOR: return 9;
+	//8 Terminales
+	case CLIPBOARD: return 17;
+	}
+	return -1;
+}
